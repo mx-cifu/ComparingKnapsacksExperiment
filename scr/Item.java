7ad3b19da2ff8e3a03861a0da6ce5c7fed4c81b1
mx-cifu <cifuentesmax1@gmail.com>
2024-02-09 16:47:48 -0800

    Added Knapsack/Item class(s)

--- scr/Item.java
+++ scr/Item.java
@@ -1,2 +1,17 @@
 public class Item {
+    private int val;
+    private int wt;
+
+    public Item(int val, int wt) {
+        this.val = val;
+        this.wt = wt;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public int getWt() {
+        return wt;
+    }
 }
